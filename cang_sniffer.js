82834ff70611db13fbf03ed65d5746b5d0d70543
Richard <richardwangwang@gmail.com>
2013-08-26 11:30:37 +0800

    Fixed cang_sniffer.js issue.

--- cang_sniffer.js
+++ cang_sniffer.js
@@ -1,11 +1,9 @@
 var cangSniffer = function(){
-    var name = "cang.baidu.com";
-    var onRequest = function(info) {
+    name: "cang.baidu.com",
+    onRequest: function(info) {
 
         return {tags: response.info.requestBody.formData.tn.join(), 
                 postingUrl: response.info.requestBody.formData.iu,
                 description: response.info.requestBody.formData.it}
-    };
-
-
+    }
 }
