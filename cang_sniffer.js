d34a0d90edef97f3f98e8cbdcad7ed5f5b3c8e76
Richard <richardwangwang@gmail.com>
2013-08-26 13:23:07 +0800

    Fixed info var.

--- cang_sniffer.js
+++ cang_sniffer.js
@@ -2,8 +2,8 @@ var cangSniffer = {
     name: "cang.baidu.com",
     onRequest: function(info) {
 
-        return {tags: response.info.requestBody.formData.tn.join(), 
-                postingUrl: response.info.requestBody.formData.iu,
-                description: response.info.requestBody.formData.it}
+        return {tags: info.requestBody.formData.tn.join(), 
+                postingUrl: info.requestBody.formData.iu,
+                description: info.requestBody.formData.it}
     }
 }
