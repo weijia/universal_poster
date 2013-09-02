29edc58f276dd883817a7cafc505c8922b5d12b8
Richard <richardwangwang@gmail.com>
2013-09-02 09:45:10 +0800

    Save item to chrome sync.

--- options.js
+++ options.js
@@ -3,24 +3,37 @@
 // Saves options to localStorage.
 function save_options(e) {
   var field = $(e.target).parents(".input-fields");
+  var savingItem = {};
   $.each($("input", field), function(key, value){
-    localStorage[value.id] = value.value;
+    //localStorage[value.id] = value.value;
+    savingItem[value.id] = value.value;
   });
   
-  // Update status to let user know options were saved.
-  var status = $(".status", field)[0];
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
+   // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set(savingItem, function() {
+    // Notify that we saved.
+    //message('Settings saved');
+    // Update status to let user know options were saved.
+    var status = $(".status", field)[0];
+    status.innerHTML = "Options Saved.";
+    setTimeout(function() {
     status.innerHTML = "";
-  }, 750);
+    }, 750);
+  });
+  
+
 }
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
   $.each($("input"), function(key, value){
     console.log(key, value, value.id, value.value);
-    if(localStorage[value.id])
-        value.value = localStorage[value.id];
+    //if(localStorage[value.id])
+    //    value.value = localStorage[value.id];
+    chrome.storage.sync.get(value.id, function(items){console.log(items);
+        if(items[value.id])
+            value.value = items[value.id];
+    });
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
