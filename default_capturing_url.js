d9701734a8203416f8a9d2831791bc42478920b3
Richard <richardwangwang@gmail.com>
2013-11-26 11:17:19 +0800

    Adding default capturing URLs.

---
+++ default_capturing_url.js
@@ -0,0 +1,4 @@
+var defaultCaptureUrls = ["http://favorite.taobao.com/popup/add_collection.htm",
+"http://base.yixun.com/json.php?mod=favor&act=add",
+"https://www.facebook.com/plugins/like/connect",
+"http://t.jd.com/product/followProduct.action"];
