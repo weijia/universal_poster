f37de8a0c54b28ffed1ed6713da6b5990c137d4c
Richard <richardwangwang@gmail.com>
2014-10-04 13:48:30 +0800

    Added test base code. Fixing notification.

--- notification.js
+++ notification.js
@@ -2,37 +2,19 @@ function showWarningTranslated(title, content) {
     
     console.log("You must first set sites you want to post to. Please open option page for this extension.");
     // Create a simple text notification:
-    var notification = webkitNotifications.createNotification(
-        '',
-    chrome.i18n.getMessage(title), //'Universal poster warning!',  // notification title
-    chrome.i18n.getMessage(content) //'You must first set sites you want to post to. Please open option page for this extension.'  // notification body text
+    createNotification(
+        chrome.i18n.getMessage(title), //'Universal poster warning!',  // notification title
+        chrome.i18n.getMessage(content) //'You must first set sites you want to post to. Please open option page for this extension.'  // notification body text
     );
-    notification.show();
-    /*var notification = chrome.notifications.create('warning', {type: "basic", title:chrome.i18n.getMessage(title),
-        message:chrome.i18n.getMessage(content)}, function(){});*/
 }
 
 
 function showMsg(title, content){
-    // Note: There's no need to call webkitNotifications.checkPermission().
-    // Extensions that declare the notifications permission are always
-    // allowed create notifications.
+    createNotification(title, content);
+}
 
-    // Create a simple text notification:
-    var notification = webkitNotifications.createNotification(
-      '',
-      title,
-      content
-    );
-    /*
-    // Or create an HTML notification:
-    var notification = webkitNotifications.createHTMLNotification(
-      'notification.html'  // html url - can be relative
-    );*/
 
-    // Then show the notification.
-    notification.show();
-    setTimeout(function(){
-      notification.cancel();
-    }, 3000);
+function createNotification(title, content){
+    var n = new Notification(title + "\n" + content);
+    setTimeout(n.close, 3000);
 }
