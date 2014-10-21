d8b6b733a5ea3cbc20225f2a881ed71467b884e5
weijia <richardwangwang@gmail.com>
2014-10-21 21:28:31 +0800

    Update default_capturing_url.js

--- default_capturing_url.js
+++ default_capturing_url.js
@@ -16,5 +16,5 @@ var defaultCaptureUrls = ["http://favorite.taobao.com/popup/add_collection.htm",
 "http://wenzhang.baidu.com/fav/add",
 "https://bitbucket.org/*/*/follow",
 "http://zhushou.huihui.cn/api/myzhushou/collection/add?",
-"ttp://www.yintai.com/ajaxpage/myfavoriteajax.aspx?op=AddItemToFavorite&itemcode="
+"http://www.yintai.com/ajaxpage/myfavoriteajax.aspx?op=AddItemToFavorite&itemcode="
 ];
