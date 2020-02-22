772b3847765ebad8606edc59fef76b9f8badd385
Richard <richard.j.wang@nokia-sbell.com>
2020-02-22 10:09:12 +0800

    Improve douban support

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-universal_poster
-================
+# universal\_poster
 
 This extension can be used to post URL automatically when using native star or add favourite opreration on many sites.
 
 For example, you may star a GIT repository in GITHUB, and this extension will automatically post it to instapaper or other supported web storage.
 
-Privacy Policy & Security
-================
+# Privacy Policy & Security
+
 What information do you collect?
 The extention do not store any personal data. The URLs which are monitored is the favarites on different sits.
 How do you use the information?
--- douban_sniffer.js
+++ douban_sniffer.js
@@ -2,13 +2,25 @@ var doubanSnifferUrlPattern = "/interest";
 var doubanSniffer = {
     sitePattern: "*://movie.douban.com/j/subject/*",
     name: "douban.com",
-    onRequest: function(info) {
+    // onRequest: function(info) {
+    //     var starUrl = info.url;
+    //     starUrl
+    //     return {tags: "collect", 
+    //             postingUrl: info.url.substring(0, info.url.length - doubanSnifferUrlPattern.length),
+    //             description: info.url.substring(0, info.url.length - doubanSnifferUrlPattern.length),
+    //             capturer: this}
+    // },
+    onRequest: function(info, callback) {
         var starUrl = info.url;
-        starUrl
-        return {tags: "collect", 
-                postingUrl: info.url.substring(0, info.url.length - doubanSnifferUrlPattern.length),
-                description: info.url.substring(0, info.url.length - doubanSnifferUrlPattern.length),
-                capturer: this}
+        chrome.tabs.get(info.tabId, function(tab){
+            var postingItem = {tags: "collect",
+                postingUrl: info.url.substring(0, info.url.length - doubanSnifferUrlPattern.length).replace("/j/subject", '/subject'),
+                description: tab.title,
+                capturer: this};
+            console.log("douban posting:", postingItem);
+            callback(postingItem);
+        });
+        return false;
     },
     matchUrl: function(info) {
         if((info.url.indexOf("movie.douban.com") != -1) &&
